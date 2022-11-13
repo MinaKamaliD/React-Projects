d1cf65a3a71d56887bf6e4dc7b3a86008de28b8a
Mina <minakamali.d@gmail.com>
2022-11-13 12:39:15 +0330

    destructuring

--- Product Card/src/Product/Product.js
+++ Product Card/src/Product/Product.js
@@ -1,14 +1,14 @@
 import "./Product.css";
 
-function Product(props) {
+function Product({ name, price, img, des }) {
   return (
     <div>
       <h2>Product Card</h2>
       <div className="card">
-        <img src={props.img} alt="" />
-        <h1>{props.name}</h1>
-        <p className="price">${props.price}</p>
-        <p>{props.des}</p>
+        <img src={img} alt="" />
+        <h1>{name}</h1>
+        <p className="price">${price}</p>
+        <p>{des}</p>
         <button>Add To Cart</button>
       </div>
     </div>
