52adb75cbab3cfed5b918c658f3ba278250f41b6
Mina <minakamali.d@gmail.com>
2022-11-13 07:11:18 +0330

    project finished

---
+++ Landing Page/src/Article/Article.js
@@ -0,0 +1,58 @@
+import "./Article.css";
+
+function Article() {
+  return (
+    <article>
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam est
+        quisquam at et perspiciatis veniam. Consectetur quod ipsum aspernatur
+        voluptate vero, quaerat corrupti rem dolor deleniti, ducimus quis
+        dignissimos iure.
+      </p>
+    </article>
+  );
+}
+export default Article;
