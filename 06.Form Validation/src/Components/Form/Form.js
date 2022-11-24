2e42db6a981d88e25ddda3559e8a53dd6cfc8ff3
Mina <minakamali.d@gmail.com>
2022-11-24 10:43:36 +0330

    project finished

--- 06.Form Validation/src/Components/Form/Form.js
+++ 06.Form Validation/src/Components/Form/Form.js
@@ -2,61 +2,107 @@ import React from "react";
 import "./Form.css";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
+    this.state = {
+      firstNameData: "",
+      lastNameData: "",
+      emailData: "",
 
-        this.state = {
-            firstNameData: '',
-            lastNameData: '',
-            emailData: '',
+      submitted: false,
+      allValid: false,
+    };
+    this.firstNameHandler = this.firstNameHandler.bind(this);
+    this.lastNameHandler = this.lastNameHandler.bind(this);
+    this.emailHandler = this.emailHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
 
-            submitted: false,
+  firstNameHandler(event) {
+    this.setState({ firstNameData: event.target.value });
+  }
+  lastNameHandler(event) {
+    this.setState({ lastNameData: event.target.value });
+  }
+  emailHandler(event) {
+    this.setState({ emailData: event.target.value });
+  }
+  submitHandler(event) {
+    event.preventDefault();
+    this.setState({ submitted: true });
 
-            allValid: false
-        }
+    if (
+      this.state.firstNameData.length !== 0 &&
+      this.state.lastNameData.length !== 0 &&
+      this.state.emailData.length !== 0
+    ) {
+      this.setState({ allValid: true });
+      setTimeout(() => {
+        this.setState({ allValid: false });
+      }, 3000);
     }
+  }
 
-
-    render() {
-        return (
-            <div className="form-container">
-                <form className="register-form" autoComplete="off">
-                    {/* Uncomment the next line to show the success message */}
-                    {/* <div className="success-message">Success! Thank you for registering</div> */}
-                    <input
-                        id="first-name"
-                        className="form-field"
-                        type="text"
-                        placeholder="First Name"
-                        name="firstName"
-                    />
-                    {/* Uncomment the next line to show the error message */}
-                    {/* <span id="first-name-error">Please enter a first name</span> */}
-                    <input
-                        id="last-name"
-                        className="form-field"
-                        type="text"
-                        placeholder="Last Name"
-                        name="lastName"
-                    />
-                    {/* Uncomment the next line to show the error message */}
-                    {/* <span id="last-name-error">Please enter a last name</span> */}
-                    <input
-                        id="email"
-                        className="form-field"
-                        type="text"
-                        placeholder="Email"
-                        name="email"
-                    />
-                    {/* Uncomment the next line to show the error message */}
-                    {/* <span id="email-error">Please enter an email address</span> */}
-                    <button className="form-field" type="submit">
-                        Register
-                    </button>
-                </form>
+  render() {
+    return (
+      <div className="form-container">
+        <form
+          className="register-form"
+          onSubmit={this.submitHandler}
+          autoComplete="off"
+        >
+          {this.state.submitted && this.state.allValid && (
+            <div className="success-message">
+              Success! Thank you for registering
             </div>
+          )}
 
-        )
-    }
+          <input
+            id="first-name"
+            className="form-field"
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            value={this.state.firstNameData}
+            onChange={this.firstNameHandler}
+          />
+          {this.state.submitted && this.state.firstNameData.length === 0 && (
+            <span id="first-name-error">Please enter a first name</span>
+          )}
+
+          <input
+            id="last-name"
+            className="form-field"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value={this.state.lastNameData}
+            onChange={this.lastNameHandler}
+          />
+          {this.state.submitted && this.state.lastNameData.length === 0 && (
+            <span id="last-name-error">Please enter a last name</span>
+          )}
+
+          <input
+            id="email"
+            className="form-field"
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={this.state.emailData}
+            onChange={this.emailHandler}
+          />
+
+          {this.state.submitted && this.state.emailData.length === 0 && (
+            <span id="email-error">Please enter an email address</span>
+          )}
+
+          <button className="form-field" type="submit">
+            Register
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
