fe790ef4077cec1e1235b4a68c635810626e65a0
Mina <minakamali.d@gmail.com>
2022-11-24 09:16:58 +0330

    projects renamed & new project created

---
+++ 06.Form Validation/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Form from "../src/Components/Form/Form";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    );
+  }
+}
