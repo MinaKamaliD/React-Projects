bf350199fe87760cd54cac25994c4e17d41055cc
Mina <minakamali.d@gmail.com>
2022-11-29 11:40:47 +0330

    part1 finished

--- 9.BookList/src/components/BookList/AddForm.js
+++ 9.BookList/src/components/BookList/AddForm.js
@@ -12,25 +12,62 @@ export default class AddForm extends Component {
       author: "",
       year: "",
     };
+
+    this.titleHandler = this.titleHandler.bind(this);
+    this.authorHandler = this.authorHandler.bind(this);
+    this.yearHandler = this.yearHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  titleHandler(event) {
+    this.setState({ title: event.target.value });
+  }
+  authorHandler(event) {
+    this.setState({ author: event.target.value });
+  }
+  yearHandler(event) {
+    this.setState({ year: event.target.value });
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
       <>
-        <form id="book-form" autocomplete="off">
+        <form id="book-form" autocomplete="off" onSubmit={this.submitHandler}>
           <div className="form-group">
             <label for="title">Title</label>
-            <input type="text" id="title" className="form-control" />
+            <input
+              type="text"
+              id="title"
+              className="form-control"
+              value={this.state.title}
+              onChange={this.titleHandler}
+            />
           </div>
 
           <div className="form-group">
             <label for="author">Author</label>
-            <input type="text" id="author" className="form-control" />
+            <input
+              type="text"
+              id="author"
+              className="form-control"
+              value={this.state.author}
+              onChange={this.authorHandler}
+            />
           </div>
 
           <div className="form-group">
             <label for="year">Year</label>
-            <input type="text" id="year" className="form-control" />
+            <input
+              type="text"
+              id="year"
+              className="form-control"
+              value={this.state.year}
+              onChange={this.yearHandler}
+            />
           </div>
           <input
             type="submit"
@@ -47,7 +84,9 @@ export default class AddForm extends Component {
             </tr>
           </thead>
           <tbody id="book-list">
-            <Book />
+            {this.state.books.map((book) => (
+              <Book />
+            ))}
           </tbody>
         </table>
       </>
