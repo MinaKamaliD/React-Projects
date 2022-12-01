6261c2c9927e505eeb29ac944214ea63d28c9705
Mina <minakamali.d@gmail.com>
2022-12-01 16:09:34 +0330

    part one finished

--- 10.Shopping Cart/src/components/Shop/Product.js
+++ 10.Shopping Cart/src/components/Shop/Product.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 
 export default class Product extends Component {
+  clickHandler(id) {
+    this.props.onAddProduct(id);
+  }
   render() {
+    let { id, title, price, img } = this.props;
+
     return (
       <div class="shop-item">
-        <span class="shop-item-title">Shirt</span>
-        <img class="shop-item-image" src="Images/Shirt.png" />
+        <span class="shop-item-title">{title}</span>
+        <img class="shop-item-image" src={img} />
         <div class="shop-item-details">
-          <span class="shop-item-price">$129</span>
-          <button class="btn btn-primary shop-item-button" type="button">
+          <span class="shop-item-price">${price}</span>
+          <button
+            class="btn btn-primary shop-item-button"
+            type="button"
+            onClick={this.clickHandler.bind(this, id)}
+          >
             ADD TO CART
           </button>
         </div>
