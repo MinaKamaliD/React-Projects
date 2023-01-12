407090b315cc573c4fb20b028aaf7e41e16683c4
Mina <minakamali.d@gmail.com>
2023-01-12 08:31:31 +0330

    project created

---
+++ 16.Course List/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Course from "./components/Course/Course";
+
+function App() {
+  return (
+    <div>
+      <Course></Course>
+    </div>
+  );
+}
+
+export default App;
