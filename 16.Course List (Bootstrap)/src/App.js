dd391c911daa7210f299a027d83bdcdbcb994920
Mina <minakamali.d@gmail.com>
2023-01-15 15:20:42 +0330

    project modified

--- 16.Course List (Bootstrap)/src/App.js
+++ 16.Course List (Bootstrap)/src/App.js
@@ -14,38 +14,38 @@ function App() {
     { id: 3, title: "CSS", src: "./img/pic3.png" },
     { id: 4, title: "Python", src: "./img/pic4.png" },
     { id: 5, title: "Vue JS", src: "./img/pic5.png" },
-    { id: 6, title: "Github", src: "./img/pic6.png" },
-    { id: 7, title: "Type Script", src: "./img/pic7.png" },
-    { id: 8, title: "Node JS", src: "./img/pic8.png" },
+    { id: 6, title: "Github", src: "./img/pic6.jpg" },
+    { id: 7, title: "Type Script", src: "./img/pic7.jpg" },
+    { id: 8, title: "Node JS", src: "./img/pic8.jpg" },
   ];
 
   return (
     <Container>
       <Row>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[0].title} src={"./img/pic2.png"}></Course>
+          <Course {...courses[0]}></Course>
         </Col>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[1].title} src={"./img/pic1.png"}></Course>
+          <Course {...courses[1]}></Course>
         </Col>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[2].title} src={"./img/pic3.png"}></Course>
+          <Course {...courses[2]}></Course>
         </Col>
 
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[3].title} src={"./img/pic4.png"}></Course>
+          <Course {...courses[3]}></Course>
         </Col>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[4].title} src={"./img/pic5.png"}></Course>
+          <Course {...courses[4]}></Course>
         </Col>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[5].title} src={"./img/pic6.jpg"}></Course>
+          <Course {...courses[5]}></Course>
         </Col>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[6].title} src={"./img/pic7.jpg"}></Course>
+          <Course {...courses[6]}></Course>
         </Col>
         <Col xs={12} sm={6} md={3}>
-          <Course title={courses[7].title} src={"./img/pic8.jpg"}></Course>
+          <Course {...courses[7]}></Course>
         </Col>
       </Row>
     </Container>
