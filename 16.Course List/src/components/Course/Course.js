5766711e8808a1422d7181ef9b0f6407dd8176a8
Mina <minakamali.d@gmail.com>
2023-01-15 12:01:35 +0330

    Card was created

--- 16.Course List/src/components/Course/Course.js
+++ 16.Course List/src/components/Course/Course.js
@@ -2,20 +2,25 @@ import React from "react";
 import { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.css";
 
 class Course extends Component {
   render() {
     return (
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src="./img/pic1.png" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>React JS</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+            repellat! Nesciunt harum debitis, nulla aut sapiente facere minima.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
         </Card.Body>
+        <div className="d-grid gap-2 rounded-3">
+          <Button variant="primary" size="lg">
+            Block level button
+          </Button>
+        </div>
       </Card>
     );
   }
